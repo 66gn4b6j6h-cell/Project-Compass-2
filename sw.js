2bc659dcf357cf9709eb15dcdf077093f68ae38f
66gn4b6j6h-cell <66gn4b6j6h@privaterelay.appleid.com>
2026-09-19 17:33:19 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,34 +1,5 @@
-const C='sakura-seoul-v7-1';
-const CORE=['./index.html','./manifest.webmanifest','./assets/maps/osaka.svg','./assets/maps/kyoto.svg','./assets/maps/fuji.svg','./assets/maps/tokyo.svg','./assets/maps/seoul.svg'];
-self.addEventListener('install',event=>{
-  self.skipWaiting();
-  event.waitUntil(caches.open(C).then(cache=>cache.addAll(CORE)));
-});
-self.addEventListener('activate',event=>{
-  event.waitUntil(Promise.all([
-    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==C).map(key=>caches.delete(key)))),
-    self.clients.claim()
-  ]));
-});
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET') return;
-  const isNavigation=event.request.mode==='navigate';
-  if(isNavigation){
-    event.respondWith(
-      fetch(event.request).then(response=>{
-        const copy=response.clone();
-        caches.open(C).then(cache=>cache.put('./index.html',copy)).catch(()=>{});
-        return response;
-      }).catch(()=>caches.match('./index.html'))
-    );
-    return;
-  }
-  event.respondWith(
-    caches.match(event.request).then(cached=>cached || fetch(event.request).then(response=>{
-      const copy=response.clone();
-      caches.open(C).then(cache=>cache.put(event.request,copy)).catch(()=>{});
-      return response;
-    }))
-  );
-});
-self.addEventListener('message',event=>{if(event.data&&event.data.type==='SKIP_WAITING') self.skipWaiting();});
+const C='sakura-seoul-v8';
+const CORE=['./','./index.html','./manifest.webmanifest','./assets/maps/osaka.svg','./assets/maps/kyoto.svg','./assets/maps/fuji.svg','./assets/maps/tokyo.svg','./assets/maps/seoul.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(CORE))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{if(e.request.method==='GET'){let y=x.clone();caches.open(C).then(c=>c.put(e.request,y)).catch(()=>{})}return x}).catch(()=>r))));

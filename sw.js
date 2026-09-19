3e82fec32cd01f250ddcbaffbca303f1a3286b2f
66gn4b6j6h-cell <66gn4b6j6h@privaterelay.appleid.com>
2026-09-19 17:43:48 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C='sakura-seoul-v8';
-const CORE=['./','./index.html','./manifest.webmanifest','./assets/maps/osaka.svg','./assets/maps/kyoto.svg','./assets/maps/fuji.svg','./assets/maps/tokyo.svg','./assets/maps/seoul.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(CORE))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{if(e.request.method==='GET'){let y=x.clone();caches.open(C).then(c=>c.put(e.request,y)).catch(()=>{})}return x}).catch(()=>r))));
+// V9 deliberately does not cache the application shell.
+// This prevents normal Safari from being pinned to an older GitHub Pages build.
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',()=>{});
